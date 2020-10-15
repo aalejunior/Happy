72c1cd194b37ba7f2833c56106a68b7b8367d9ee
Juniorkk <junior.alexandre.ja95@gmai.com>
2020-10-15 12:20:33 -0300

    add nodemon

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log('roi, leticia ne')
+const express = require('express');
+const path = require('path');
+
+
+//iniciando o express
+const server = express()
+server
+.use(express.static('public'))
+
+//criando uma rota
+.get('/', (request, response) => {
+    return response.sendFile(path.join(__dirname, 'views','index.html'))
+})
+
+//ligando o servidor 
+server.listen(5500)
