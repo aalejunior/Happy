d281e1ad6ae853e2078c8bebded559d4a6607bd7
Juniorkk <junior.alexandre.ja95@gmai.com>
2020-10-15 14:42:14 -0300

    start orphanages backend
    hype ;)

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -8,21 +8,39 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
 
 //Criando icone
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
+    iconUrl: "/images/map-marker.svg",
     iconSize:[58,68],
     iconAnchor:[29,68],
     popupAnchor:[170,2]
 })
 
-//Criando popup 
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeigth: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg">  </a>')
 
-L.marker([-27.222633, -49.6455874], { icon })
-    .addTo(map)
-    .bindPopup(popup)
+function addMarker({id, name, lat, lng}) {
+
+    //Criando popup 
+    const popup = L.popup({
+        closeButton: false,
+        className: 'map-popup',
+        minWidth: 240,
+        minHeigth: 240
+    }).setContent(`${name} <a href="/orphanage?id=${id}"> <img src="/images/arrow-white.svg">  </a>`)
+    
+    L.marker([lat,lng], { icon })
+        .addTo(map)
+        .bindPopup(popup)
+
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+orphanagesSpan.forEach(span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng
+    }
+
+    addMarker(orphanage)
+})
+
     
--- src/server.js
+++ src/server.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const path = require('path');
-
+const pages = require('./pages.js')
 
 //iniciando o express
 const server = express()
 server
 .use(express.static('public'))
 
-//criando uma rota
-.get('/', (request, response) => {
-    return response.sendFile(path.join(__dirname, 'views','index.html'))
-})
+
+//confirugando a tamplate engine
+.set('views', path.join(__dirname, "views"))
+.set('view engine', 'hbs')
+
+
+//rotas da aplição
+.get('/', pages.index)
+.get('/orphanage', pages.orphanage)
+.get('/orphanages', pages.orphanages)
+.get('/create-orphanage', pages.createOrphanage)
+
+
+
 
 //ligando o servidor 
 server.listen(5500)
