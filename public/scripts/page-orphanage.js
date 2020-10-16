499c6d35b008e1765cde4314f7ac02061782c5d1
Juniorkk <junior.alexandre.ja95@gmai.com>
2020-10-16 12:42:21 -0300

    finish page orphanades

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -6,9 +6,14 @@ const options = {
     zoomControl: false
 }
 
+
+//valuaes
+const lat = document.querySelector(' span[data-lat]').dataset.lat
+const lng = document.querySelector(' span[data-lng]').dataset.lng
+
 /* Criando mapa*/
 
-const map = L.map('mapid', options).setView([-27.222633, -49.6455874], 15);
+const map = L.map('mapid', options).setView([lat, lng], 15);
 
 /* Criando e adicionando capada de mapa*/
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
@@ -24,7 +29,9 @@ const icon = L.icon({
 
 //Criando popup 
 
-L.marker([-27.222633, -49.6455874], { icon })
+
+
+L.marker([lat, lng], { icon })
     .addTo(map)
 
 
