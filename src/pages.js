b62c002e0dfe5bbdffff78fe1c9861b1d66ce13c
Juniorkk <junior.alexandre.ja95@gmai.com>
2020-10-16 13:41:55 -0300

    finaly end backend form

--- src/pages.js
+++ src/pages.js
@@ -22,9 +22,9 @@ module.exports = {
             orphanage.images = orphanage.images.split(",");
             orphanage.firstImage = orphanage.images[0];
 
-            if(orphanage.open_on_weekends == "0"){
+            if (orphanage.open_on_weekends == "0") {
                 orphanage.open_on_weekends = false
-            }else{
+            } else {
                 orphanage.open_on_weekends = true
             }
 
@@ -49,4 +49,37 @@ module.exports = {
     createOrphanage(req, res) {
         return res.render("create-orphanage");
     },
+
+    async saveOrphanage(req, res) {
+        const fields = req.body
+
+        //validação das marcaçoes
+        if (Object.values(fields).includes('')) {
+            return res.send('Todos os campos devem ser preenchidos!')
+        }
+
+        try {
+            //salvar um orfanato
+            const db = await Database;
+            await saveOrphanage(db, {
+                lat: fields.lat,
+                lng: fields.lng,
+                name: fields.name,
+                about: fields.about,
+                whatsapp: fields.whatsapp,
+                images: fields.images.toString(),
+                instructions: fields.instructions,
+                opening_hours: fields.opening_hours,
+                open_on_weekends: fields.open_on_weekends,
+
+            })
+            //redirecionar
+            return res.redirect('/orphanages')
+        } catch (error) {
+            console.log(error);
+            return res.send("Erro no banco de dados");
+
+        }
+
+    }
 };
--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
+const { orphanage } = require('./pages.js');
 const pages = require('./pages.js')
 
 //iniciando o express
 const server = express()
 server
+
+//usar o corpo da requisição
+.use(express.urlencoded({extended: true}))
+
 .use(express.static('public'))
 
 
@@ -18,6 +23,7 @@ server
 .get('/orphanage', pages.orphanage)
 .get('/orphanages', pages.orphanages)
 .get('/create-orphanage', pages.createOrphanage)
+.post('/save-orphanage', pages.saveOrphanage)
 
 
 
